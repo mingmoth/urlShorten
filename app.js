e9acc66f562b22db67cd8ee41dfa907e1063bdfd
mingmoth <mingmoth@gmail.com>
2021-08-03 22:24:40 +0800

    feat: dev basic layout

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const Url = require('./models/url')
+const exphbs = require('express-handlebars')
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/urlShorten', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongpdb connected')
+})
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('urlShortener')
+  res.render('index')
 })
 
 app.listen(3000, () => {
